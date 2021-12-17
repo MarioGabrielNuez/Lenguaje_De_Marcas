533aeebf1c4b339733cb106b15def0cbe6ff5e27
MarioGabIFP <IFPmarionunezinfor@gmail.com>
2021-12-17 15:50:51 +0100

    Creación nuevo proyecto

---
+++ Proyecto 9/app.js
@@ -0,0 +1,5 @@
+function ratonEncima() {
+    let button = document.getElementById('button');
+    button.style.marginTop = "10%";
+    button.onmouseleave="document.getElementById('button').style.marginTop = '0%'"
+};
